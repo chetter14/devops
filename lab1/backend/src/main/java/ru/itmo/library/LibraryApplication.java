2df03a99499558a26a4f6ff77b7a2f3092fcda66
Artem Leshchukov <artemleshchukov02@gmail.com>
2025-06-10 18:58:01 +0300

    Comment out the meaningless and useless code

--- lab1/backend/src/main/java/ru/itmo/library/LibraryApplication.java
+++ lab1/backend/src/main/java/ru/itmo/library/LibraryApplication.java
@@ -7,60 +7,60 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
     // Unused field
-    private static final String UNUSED_FIELD = "This is never used";
+//    private static final String UNUSED_FIELD = "This is never used";
 
     public static void main(String[] args) {
-        // Duplicate strings
-        String s1 = "SonarQube";
-        String s2 = "SonarQube";
-
-        // Empty catch block
-        try {
-            int num = Integer.parseInt("123");
-        } catch (NumberFormatException e) {
-            // Do nothing
-        }
-
-        // Potential NPE
-        String nullString = null;
-        if (nullString.equals("test")) {
-            System.out.println("This will never happen");
-        }
-
-        // Unused variable
-        int unusedVar = 42;
-
-        // Inefficient string concatenation in loop
-        String result = "";
-        for (int i = 0; i < 10; i++) {
-            result += i;
-        }
-
-        // Hardcoded password - security hotspot
-        String password = "admin123";
-
-        // Random unused method call
-        meaninglessMethod();
+//        // Duplicate strings
+//        String s1 = "SonarQube";
+//        String s2 = "SonarQube";
+//
+//        // Empty catch block
+//        try {
+//            int num = Integer.parseInt("123");
+//        } catch (NumberFormatException e) {
+//            // Do nothing
+//        }
+//
+//        // Potential NPE
+//        String nullString = null;
+//        if (nullString.equals("test")) {
+//            System.out.println("This will never happen");
+//        }
+//
+//        // Unused variable
+//        int unusedVar = 42;
+//
+//        // Inefficient string concatenation in loop
+//        String result = "";
+//        for (int i = 0; i < 10; i++) {
+//            result += i;
+//        }
+//
+//        // Hardcoded password - security hotspot
+//        String password = "admin123";
+//
+//        // Random unused method call
+//        meaninglessMethod();
 
         SpringApplication.run(LibraryApplication.class, args);
     }
 
-    // Method with multiple issues
-    private void neverCalled() {
-        // Duplicated code
-        System.out.println("Hello");
-        System.out.println("Hello");
-
-        // Magic number
-        for (int i = 0; i < 100; i++) {
-            if (i == 99) {
-                System.out.println("Magic number reached");
-            }
-        }
-    }
-
-    // Dead code
-    private static void meaninglessMethod() {
-        System.out.println("This will never be executed");
-    }
+//    // Method with multiple issues
+//    private void neverCalled() {
+//        // Duplicated code
+//        System.out.println("Hello");
+//        System.out.println("Hello");
+//
+//        // Magic number
+//        for (int i = 0; i < 100; i++) {
+//            if (i == 99) {
+//                System.out.println("Magic number reached");
+//            }
+//        }
+//    }
+//
+//    // Dead code
+//    private static void meaninglessMethod() {
+//        System.out.println("This will never be executed");
+//    }
 }
